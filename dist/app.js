75c25b73a195eb098060be8b32bd3be81acb6c06
Matteo Spinelli <matteospinelli10@gmail.com>
2023-04-19 17:47:29 +0200

    add ex

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+const app = express();
+app.listen(process.env.PORT, () => console.log(`Server listening at port:${process.env.PORT}`));
