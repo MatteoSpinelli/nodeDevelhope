a01e5707f7d6c546150c728d719fe55ac60b6d24
Matteo Spinelli <matteospinelli10@gmail.com>
2023-05-18 11:55:44 +0200

    add ex

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,72 +1,78 @@
-import { Response, Request } from "express";
-import { db } from "../app.js";
+import type { Request, Response } from "express";
+import Joi from "joi";
+
+import db from "../lib/db.js";
+
 type Planet = {
-    id: number,
-    name: string,
-};
+  id?: number;
+  name: string;
+} | null;
 
-type Planets = Planet[];
-let planets: Planets = [
-    {
-        id: 1,
-        name: "Earth",
-    },
-    {
-        id: 2,
-        name: "Mars",
-    },
-];
-
+const planetSchema = Joi.object({
+  name: Joi.string().required(),
+});
 
 const getAll = async (req: Request, res: Response) => {
-    const planets = await db.many("SELECT * FROM planets")
-    res.status(200).json(planets)
-}
+  const planets: Planet[] = await db.many(`SELECT * FROM planets`);
+  res.status(200).json(planets);
+};
 
 const getOneById = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const planet = await db.oneOrNone("SELECT * FROM planets WHERE id=$1", id)
-    planet ? res.status(200).json(planet) : res.status(401).json({ msg: "planet not found" })
-}
+  const { id } = req.params;
+  const planet: Planet = await db.oneOrNone(
+    "SELECT * from planets WHERE id=$1",
+    Number(id)
+  );
+  res.status(200).json(planet || { msg: "There are no planets" });
+};
+
 const create = async (req: Request, res: Response) => {
-    const { name } = req.body
-    if (!name) {
-        res.status(400).json({ msg: "you have to provide a name!" })
-        return
-    }
-    await db.none("INSERT INTO planets (name) VALUES ($1)", name)
-    res.status(201).json({ msg: "planet created"})
-}
+  const { name } = req.body;
+  const newPlanet = { name };
+  const validatedPlanet = planetSchema.validate(newPlanet);
+
+  if (validatedPlanet.error) {
+    res.status(400).json({ msg: validatedPlanet.error });
+    return;
+  }
+
+  await db.none("INSERT INTO planets (name) VALUES ($1)", name);
+  res.status(201).json({ msg: `Planet ${name} created` });
+};
+
 const updateById = async (req: Request, res: Response) => {
-    const { id, name } = req.body
-    if (!id) {
-        res.status(400).json({ msg: "you have to provide an id!" })
-        return
-    }
-    await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
+  const id = Number(req.params.id);
+  const { name } = req.body;
+  const updPlanet: Planet = { id, name };
+  const validatedPlanet = planetSchema.validate(updPlanet);
 
-    res.status(200).json({ msg: "planet updated" })
-}
+  if (validatedPlanet.error) {
+    res.status(400).json({ msg: validatedPlanet.error });
+    return;
+  }
+
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [Number(id), name]);
+  res.status(200).json({ msg: `Planet ${id} updated` });
+};
+
 const deleteById = async (req: Request, res: Response) => {
-    const { id } = req.body
-    if (!id) {
-        res.status(400).json({ msg: "you have to provide an id!" })
-        return
-    }
-    await db.none("DELETE FROM planets WHERE id=$1", id)
+  const { id } = req.params;
+  await db.none("DELETE FROM planets WHERE id=$1", Number(id));
+  res.status(200).json({ msg: `Planet ${id} deleted` });
+};
 
-    res.status(200).json({ msg: "planet deleted" })
-}
-const uploadImg = async (req: any, res: Response) => {
-    const { id } = req.params
-    await db.none("UPDATE planets SET image=$2 WHERE id=$1", [id, req.file.filename])
-    res.status(200).json({ msg: "image uploaded" })
-}
-export {
-    getAll,
-    getOneById,
-    create,
-    updateById,
-    deleteById,
-    uploadImg
-}
+const createImage = async (req: Request, res: Response) => {
+  const { file } = req;
+  const { id } = req.params;
+  const fileName = file?.path;
+
+  if (!fileName) {
+    res.status(400).json({ msg: "Planet image upload failed" });
+    return;
+  }
+
+  db.none("UPDATE planets SET image=$2 WHERE id=$1", [Number(id), fileName]);
+  res.status(201).json({ msg: "Planet image uploaded" });
+};
+
+export { getAll, getOneById, create, updateById, deleteById, createImage };
